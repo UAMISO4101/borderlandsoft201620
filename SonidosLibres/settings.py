e9c9b14aa3c9b8a5a5fff2a19d952debc020718a
dhmahecha <dh.mahecha@uniandes.edu.co>
2016-09-27 23:52:15 -0500

    Ajustes BD

--- SonidosLibres/settings.py
+++ SonidosLibres/settings.py
@@ -76,14 +76,14 @@ WSGI_APPLICATION = 'SonidosLibres.wsgi.application'
 
 DATABASES = {
     'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.mariadb',
-        'NAME': 'SonidosLibres',
-        'USER': os.environ['MYSQL_USER'],
-        'PASSWORD': os.environ['MYSQL_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.mariadb',
+        #'NAME': 'SonidosLibres',
+        #'USER': os.environ['MYSQL_USER'],
+        #'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        #'HOST': 'localhost',
+        #'PORT': '3306',
     }
 }
 

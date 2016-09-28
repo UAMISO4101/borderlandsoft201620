de1cdcbb7a8b840914cb05687435fbe88904b0a9
dhmahecha <dh.mahecha@uniandes.edu.co>
2016-09-27 21:51:23 -0500

    Configuración inicialde base de datos

--- SonidosLibres/settings.py
+++ SonidosLibres/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'contenido.apps.ContenidoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,14 @@ WSGI_APPLICATION = 'SonidosLibres.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mariadb',
+        'NAME': 'SonidosLibres',
+        'USER': os.environ['MYSQL_USER'],
+        'PASSWORD': os.environ['MYSQL_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- contenido/models.py
+++ contenido/models.py
@@ -1,3 +1,50 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Artista(models.Model):
+    """
+    Describe un artista.
+    """
+    nom_artistico = models.CharField(max_length=200)
+    nom_pais = models.CharField(max_length=50)
+    nom_ciudad = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nom_artistico
+
+class Audio(models.Model):
+    """
+    Describe un audio.
+    """
+    nom_audio = models.CharField(max_length=1000, verbose_name='Audio', help_text='Nombre del audio')
+    val_imagen = models.CharField(max_length=1000, verbose_name='Imágen', help_text='URL de la imagen del audio')
+    val_recurso = models.CharField(max_length=1000, verbose_name='Recurso', help_text='URL del recurso del audio')
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nom_audio
+
+class Album(models.Model):
+    """
+    Describe un album.
+    """
+    val_imagen = models.CharField(max_length=1000, verbose_name='Imágen', help_text='URL de la imagen del album')
+    nom_album = models.CharField(max_length=1000, verbose_name='Album', help_text='Nombre del album')
+
+    def __str__(self):
+        return self.nom_album
+
+
+class Artista_album(models.Model):
+    """
+    Describe la relación entre artista y album
+    """
+    artista = models.ForeignKey(Artista, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+
+class Album_audio(models.Model):
+    """
+    Describe la relación entre album y audio
+    """
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    audio = models.ForeignKey(Audio, on_delete=models.CASCADE)

ca253825f3497df7d95fb75810824ac695ba6d55
dhmahecha <dh.mahecha@uniandes.edu.co>
2016-09-28 23:03:02 -0500

    requerimiento Albumes Historia de usuario H006 primera parte

--- contenido/urls.py
+++ contenido/urls.py
@@ -5,5 +5,7 @@ app_name = 'contenido'
 
 
 urlpatterns = [
-    url(r'^albumes$', views.AlbumView.as_view(), name="lista_albumes"),
+    url(r'^$', views.AlbumesView.as_view(), name='index'),
+    url(r'^albumes/(?P<artista_id>[0-9]+)/$', views.AlbumesView.as_view(), name='album'),
+    #url(r'^albumes$', views.AlbumView.as_view(), name="lista_albumes"),
 ]
--- contenido/views.py
+++ contenido/views.py
@@ -5,11 +5,23 @@ from .models import *
 
 # Create your views here.
 
-class AlbumView(ListView):
-    template_name = 'contenido/album.html'
-    context_object_name = 'lista_albumes'
-    queryset = Album.objects.all()
-    #serializer_class = DeporteSerializer
+#class AlbumView(ListView):
+#    template_name = 'contenido/albumes.html'
+#    context_object_name = 'lista_albumes'
+
+#    def get_queryset(self):
+#        return Album.objects.all()
+
+class AlbumesView(ListView):
+    template_name = 'contenido/albumes.html'
+    context_object_name = 'lista_artista_album'
+    artista = Artista
 
     def get_queryset(self):
-        return Album.objects.all()
+        self.artista = get_object_or_404(Artista, id=int(self.kwargs['artista_id']))
+        return Artista_album.objects.filter(artista__pk=self.artista.pk)
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumesView, self).get_context_data(**kwargs)
+        context['artista'] = self.artista
+        return context

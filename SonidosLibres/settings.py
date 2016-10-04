447696638ec9c7eb00b2e9c548813c250c22a805
jcastrov <johcavi@gmail.com>
2016-10-03 23:59:39 -0500

    Se agregó la ruta manualmente STATICFILES_DIRS

--- SonidosLibres/settings.py
+++ SonidosLibres/settings.py
@@ -89,14 +89,14 @@ WSGI_APPLICATION = 'SonidosLibres.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': str(PROJECT_PACKAGE.joinpath('db.sqlite3')),  # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'SonidosLibres',
-        'USER': os.environ['POSTGRES_USER'],
-        'PASSWORD': os.environ['POSTGRES_PASSWORD'],
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(PROJECT_PACKAGE.joinpath('db.sqlite3')),  # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'SonidosLibres',
+        # 'USER': os.environ['POSTGRES_USER'],
+        # 'PASSWORD': os.environ['POSTGRES_PASSWORD'],
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
@@ -155,12 +155,17 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/staticfiles/'
 
-# Extra places for collectstatic to find static files.
+# Nombre de la carpeta que crea
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+
+# Ruta que apunta los archivos en el front
+STATIC_URL = '/s/'
+
+# Carpeta donde se encuentran los archivos estáticos
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    '/app/SonidosLibres/staticfiles',
 )
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'

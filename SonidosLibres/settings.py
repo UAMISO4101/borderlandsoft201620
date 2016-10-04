661be7b03fcfe4dc219f5d14d885a4e9e8d88448
jcastrov <johcavi@gmail.com>
2016-10-03 23:21:05 -0500

    Se renombra de static a staticfiles

--- SonidosLibres/settings.py
+++ SonidosLibres/settings.py
@@ -156,7 +156,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (

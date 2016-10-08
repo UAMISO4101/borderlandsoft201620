496d90246c53032bcceb83c989b5aafe9cdffe40
jcastrov <johcavi@gmail.com>
2016-10-08 16:17:10 -0500

    Se agregó Debug = false a prod.py

--- SonidosLibres/settings/prod.py
+++ SonidosLibres/settings/prod.py
@@ -1,6 +1,8 @@
 from .common import *
 import dj_database_url
 
+DEBUG = False
+
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)

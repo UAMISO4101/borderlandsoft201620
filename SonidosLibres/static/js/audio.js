5a97abb1304a0d05013650e688780dbd7faeda47
jcastrov <johcavi@gmail.com>
2016-10-23 14:26:45 -0500

    Se aplicaron buenas prácticas para la implementación de JavaScript en Django

--- SonidosLibres/static/js/audio.js
+++ SonidosLibres/static/js/audio.js
@@ -1,23 +1,111 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip();
-  $("[data-hover='tooltip']").tooltip();
-})
+    $('[data-toggle="tooltip"]').tooltip();
+    $("[data-hover='tooltip']").tooltip();
+    getAudios();
+});
 
-function test(song_id){
+function getAudios() {
+    var songId = backVars.songId; // $("#songId").val();
+    $.ajax({
+        type:"GET",
+        contentType:"application/json; charset=utf8",
+        url:"/api/audios/" + songId + "/?format=json",
+        success:function (response) {
+            var listArtistas = response.artistas;
+            var listAlbums = response.albums;
+            if(listArtistas != null && listArtistas != undefined && listArtistas != "") {
+                for (var i = 0; i <= listArtistas.length - 1; i++) {
+                    getArtistas(listArtistas[i]);
+                }
+            }
 
-  $.ajax({
-    type:"POST",
-    url:"/like/",
-    data: {
-      'song_id': song_id
-    },
-    success: function(data){
-      if(data == "SUCCESS"){
-         location.reload();
-      }
-    },
-    error: function (data) {
-      alert("Se produjo un error inesperado :(")
+            if(listAlbums != null && listAlbums != undefined && listAlbums != "") {
+                for (i = 0; i <= listArtistas.length - 1; i++) {
+                    getAlbums(listAlbums[i]);
+                }
+            }
+            $('#spanNumArtistas').append(listArtistas.length);
+            $('#spanNumAlbums').append(listAlbums.length);
+        }
+
+    });
+}
+
+function getArtistas(artista) {
+    var conteoObras = JSON.parse(getConteoAudiosByArtista(artista.id)).length;
+    var divArtistas = '<div class="media">' +
+        '<div class="media-left">';
+    if(artista.val_imagen == null || artista.val_imagen == "") {
+        divArtistas = divArtistas + '<div class="detail-img artist-img si50 text-center">' +
+            '<i class="fa fa-user"></i>' +
+            '</div>';
     }
-  });
+    else {
+        divArtistas = divArtistas + '<img class="detail-img si50" src="'+artista.val_imagen+'">';
+    }
+    divArtistas = divArtistas +  '</div>' +
+        '<div class="media-body">' +
+        '<h4 class="media-heading"><a href="/user/'+ artista.id + '">'+ artista.nom_artistico +'</a></h4>' +
+        conteoObras + ' obras' +
+        '</div>' +
+        '</div>';
+
+
+    $('#divArtistas').append(divArtistas);
+}
+
+function getConteoAudiosByArtista(idArtista) {
+    var count = 0;
+    $.ajax({
+        type:"GET",
+        contentType:"application/json; charset=utf8",
+        url:"/api/audiosbyartista/"+idArtista+"/?format=json",
+        success: function (response) {
+            count = response;
+        }
+    });
+    return count;
+}
+
+function getAlbums(album) {
+    var divAlbums = '<div class="col-xs-6 col-sm-4 col-md-3 col-lg-2">'+
+        '<div class="box album-box">'+
+        '<a href="/album/'+ album.id + '">';
+    if(album.val_imagen == null || album.val_imagen == undefined || album.val_imagen.trim() == "") {
+        divAlbums = divAlbums + '<div class="detail-img album-img album-big text-center">' +
+            '<i class="fa fa-music"></i>' +
+            '<i class="fa fa fa-circle-thin"></i>' +
+            '</div>';
+    }
+    else{
+        divAlbums = divAlbums + '<img class="detail-img album-img album-big" src="' + album.val_imagen + '"  >';
+    }
+    divAlbums = divAlbums + '<div class="details-box">'+
+        '<span>'+album.nom_album+'</span>'+
+        '<br>'+
+        '<div class="text-right"><small>'+new Date(album.fec_creacion_album).getFullYear()+'</small></div>'+
+        '</div>'+
+        '</a>'+
+        '</div>'+
+        '</div>';
+
+    $('#divAlbums').append(divAlbums);
+
+}
+function test(song_id){
+    $.ajax({
+        type:"POST",
+        url:"/like/",
+        data: {
+            'song_id': song_id
+        },
+        success: function(data){
+            if(data == "SUCCESS"){
+                location.reload();
+            }
+        },
+        error: function () {
+            alert("Se produjo un error inesperado :(")
+        }
+    });
 }

0d884d8d5a5860da8e3739134b1e6b73ac9e33c0
lstalero21 <ls.talero21@uniandes.edu.co>
2016-10-31 22:05:03 -0500

    Ajuste settings para CI

--- SonidosLibres/settings/common.py
+++ SonidosLibres/settings/common.py
@@ -112,7 +112,7 @@ STATIC_ROOT = str(PROJECT_ROOT.joinpath('staticfiles')) # os.path.join(PROJECT_R
 # Ruta que apunta los archivos en el front
 STATIC_URL = '/static/'
 
-# Carpeta donde se encuentran los archivos estáticos
+# Carpeta donde se encuentran los archivos estaticos
 STATICFILES_DIRS = (
     str(PROJECT_ROOT.joinpath('static')),
 )

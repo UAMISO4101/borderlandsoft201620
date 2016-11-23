f989d6f2a7a015a4c47483982ca0d9d965ed0694
jcastrov <johcavi@gmail.com>
2016-11-23 15:03:46 -0500

    Paso 1: Test para probar si abre el pop up de agregar album (Rojo)

---
+++ contenido/tests/HU039_test.py
@@ -0,0 +1,28 @@
+# - *- coding: utf-8 - *-
+
+from unittest import TestCase
+from selenium import webdriver
+from contenido.tests.util import Util
+
+
+class HU039Test(TestCase):
+    def setUp(self):
+        self._util = Util(site=self)
+        self._username = 'lindsey'
+        self._password = 'qqwwqqwwq'
+        self.browser = webdriver.Firefox()
+        self.browser.get('http://localhost:8000/accounts/login')
+
+    # def tearDown(self):
+
+    def login(self):
+        self._util.set_text('txtUser', self._username)
+        self._util.set_text('txtPassword', self._password)
+        self._util.set_click('btnLogin')
+        self.browser.get('http://localhost:8000/song/5/')
+
+    def abrir_modal_agregar_audio_a_album(self):
+        self.login()
+        self._util.set_click('addToAlbumButton')
+        self.browser.implicitly_wait(2)
+        self.assertIn(self._util.get_class_attr("addToAlbumModal"), "modal fade in")

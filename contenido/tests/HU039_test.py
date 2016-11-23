ab48218e55f50b14bd5c1cfc838acc50c503c8ca
jcastrov <johcavi@gmail.com>
2016-11-23 15:27:24 -0500

    Paso 3: Test para probar si el artista no agrega audios a albums que no le pertenecen (Rojo)

--- contenido/tests/HU039_test.py
+++ contenido/tests/HU039_test.py
@@ -2,6 +2,7 @@
 
 from unittest import TestCase
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from contenido.tests.util import Util
 
 
@@ -15,20 +16,29 @@ class HU039Test(TestCase):
 
     # def tearDown(self):
 
-    def login(self):
+    def login(self, audio_id):
         self._util.set_text('txtUser', self._username)
         self._util.set_text('txtPassword', self._password)
         self._util.set_click('btnLogin')
-        self.browser.get('http://localhost:8000/song/5/')
+        self.browser.get('http://localhost:8000/song/' + audio_id)
 
-    def abrir_modal_agregar_audio_a_album(self):
-        self.login()
+    def abrir_modal_agregar_audio_a_album(self, audio_id):
+        self.login(audio_id)
         self._util.set_click('addToAlbumButton')
         self.browser.implicitly_wait(2)
         self.assertIn(self._util.get_class_attr("addToAlbumModal"), "modal fade in")
 
     def abrir_y_cerrar_modal_agregar_audio_a_album(self):
-        self.abrir_modal_agregar_audio_a_album()
+        self.abrir_modal_agregar_audio_a_album('5')
         self.browser.implicitly_wait(2)
         self._util.set_click('btnCloseAddToAlbumModal')
         self.assertIn("modal fade", self._util.get_class_attr("addToAlbumModal"))
+
+    def verificar_si_puede_editar_otros_albums(self):
+        self.login('6')
+        self.browser.implicitly_wait(2)
+        try:
+            self.browser.find_element_by_id('addToAlbumButton')
+        except NoSuchElementException:
+            return True
+        return False
